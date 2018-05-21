95a3bba4b12ed1cec509ca73cc1a2d1f31b4e935
LeoHu <anthony_hu@hotmail.com>
2018-05-21 21:34:44 +0800

    git checkout 2-fs

--- os/bootloader/target/aarch64-none-elf/release/deps/volatile-962006f5188c39f2.d
+++
@@ -1,7 +0,0 @@
-/home/leohu/Documents/cs140e/os/bootloader/target/aarch64-none-elf/release/deps/libvolatile-962006f5188c39f2.rlib: /home/leohu/Documents/cs140e/os/volatile/src/lib.rs /home/leohu/Documents/cs140e/os/volatile/src/traits.rs /home/leohu/Documents/cs140e/os/volatile/src/macros.rs
-
-/home/leohu/Documents/cs140e/os/bootloader/target/aarch64-none-elf/release/deps/volatile-962006f5188c39f2.d: /home/leohu/Documents/cs140e/os/volatile/src/lib.rs /home/leohu/Documents/cs140e/os/volatile/src/traits.rs /home/leohu/Documents/cs140e/os/volatile/src/macros.rs
-
-/home/leohu/Documents/cs140e/os/volatile/src/lib.rs:
-/home/leohu/Documents/cs140e/os/volatile/src/traits.rs:
-/home/leohu/Documents/cs140e/os/volatile/src/macros.rs:
--- os/bootloader/target/release/build/bootloader-dc1b1e4c9d144f62/build_script_build-dc1b1e4c9d144f62.d
+++
@@ -1,5 +0,0 @@
-/home/leohu/Documents/cs140e/os/bootloader/target/release/build/bootloader-dc1b1e4c9d144f62/build_script_build-dc1b1e4c9d144f62: build.rs
-
-/home/leohu/Documents/cs140e/os/bootloader/target/release/build/bootloader-dc1b1e4c9d144f62/build_script_build-dc1b1e4c9d144f62.d: build.rs
-
-build.rs:
