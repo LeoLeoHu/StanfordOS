43561be87e92032d34a2ecfd5480439697d66567
LeoHu <anthony_hu@hotmail.com>
2018-05-21 15:09:06 +0800

    checkout

--- os/pi/src/common.rs
+++ os/pi/src/common.rs
@@ -1,3 +1,16 @@
+<<<<<<< HEAD
+/// The address where I/O peripherals are mapped to.
+pub const IO_BASE: usize = 0x3F000000;
+
+/// Generates `pub enums` with no variants for each `ident` passed in.
+pub macro states($($name:ident),*) {
+    $(
+        /// A possible state.
+        #[doc(hidden)]
+        pub enum $name {  }
+    )*
+}
+=======
 /// The address where I/O peripherals are mapped to.
 pub const IO_BASE: usize = 0x3F000000;
 
@@ -5,3 +18,4 @@ pub const IO_BASE: usize = 0x3F000000;
 pub macro states($($name:ident),*) {
     $(pub enum $name {  })*
 }
+>>>>>>> master
